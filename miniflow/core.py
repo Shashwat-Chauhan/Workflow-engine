1cb6c545947103f8274103b597893b3eab808c11
shashwat-chauhan <shashwatchauhan12@gmail.com>
2026-03-09 23:34:40 +0530

    created a workflowmeta class , and reconfigures the step decorator

--- miniflow/core.py
+++ miniflow/core.py
@@ -13,14 +13,22 @@ logger = get_logger("miniflow.core")
 @dataclass
 class StepMeta:
     name: str
-    fn : Callable[...,Any]
+    fn : Callable[..., Any]
     retries : int = 0
-    order: int = 0
+    order : int = 0
 
-    def step(order: int = 0 , retries: int = 0):
-        def decorator(fn):
-            fn.__miniflow_step__ = StepMeta(name = fn.__name__ , fn = fn , retries=retries , order=order)
-            return fn
+
+def Step(order: int = 0 , retries : int = 0):
+    def decorator(fn):
+        fn.__miniflow_step__ = StepMeta(
+            name = fn.__name__,
+            fn = fn,
+            reties = retries,
+            order = order
+        )
+        return fn
+    return decorator
+
 
 @dataclass
 class RunContext:
@@ -29,3 +37,15 @@ class RunContext:
     step_index: int = 0
     status: str = "created"  # running, success, failed
     metadata: Dict[str, Any] = field(default_factory=dict)
+
+
+class WorkflowMeta(type):
+    def __new__(mcls, name, bases, attrs):
+        steps: List[StepMeta] = []
+        for key, val in list(attrs.items()):
+            if hasattr(val, "__miniflow_step__"):
+                meta: StepMeta = getattr(val, "__miniflow_step__")
+                steps.append(meta)
+        steps.sort(key=lambda s: s.order)
+        attrs["_miniflow_steps"] = steps
+        return super().__new__(mcls, name, bases, attrs)
